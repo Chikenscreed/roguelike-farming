8e9c9352a3006c9104e45ac075c018406a2ce985
Jasmin Hoffmann <jasminhoffmann436@gmail.com>
2026-03-03 19:11:59 +0100

    placed Tiles can be transformed into dictionary containing tileData. Should be used to generate the Rooms

--- roguelike-farming/Scripts/build_dungeon_screen.gd
+++ roguelike-farming/Scripts/build_dungeon_screen.gd
@@ -1,7 +1,8 @@
 extends Control
 
 
-var currentlyHoveredOverSlot: MapTile
+@onready var tile_frame_2: Control = $TileFrame2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,3 +17,22 @@ func _process(delta: float) -> void:
 func _on_frame_tile_slot_area_entered(area: Area2D) -> void:
 	print("something entered")
 	pass # Replace with function body.
+
+#this dunction wopuld have to transform the slots into a dictionary with Vector2 as keys?
+func submitDungeon() -> void: 
+	var counter = 0
+	var allRooms: Dictionary = {}
+	var x: int = 0
+	var y: int =0
+	for room in tile_frame_2.get_children():
+		room = room as MapTile
+		x = counter /3
+		y = counter % 3
+		allRooms.set(Vector2(x,y), room.tileData)
+		counter += 1
+	print("Transformed all")
+	pass
+
+
+func _on_button_pressed() -> void:
+	submitDungeon()
---
+++ roguelike-farming/dungeon_manager.gd
@@ -0,0 +1,14 @@
+class_name DungeonManager
+extends Node2D
+
+
+##This class should know how the rooms are arranged and in which room the player currently is. 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
