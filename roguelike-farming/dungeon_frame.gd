78602e39838e273c6b494230fc5579a97d5cc468
Jasmin Hoffmann <jasminhoffmann436@gmail.com>
2026-03-17 19:40:23 +0100

    POI get generated. Number according to Dungeon Size. Currently only chests

--- roguelike-farming/dungeon_frame.gd
+++ roguelike-farming/dungeon_frame.gd
@@ -16,6 +16,7 @@ func _ready() -> void:
 	var entranceTile: Vector2i = showDungeonEntrance()
 	while (entranceTile == exitTile):
 		entranceTile = showDungeonEntrance()
+	placePOITiles([exitTile, entranceTile])
 	startRoom = entranceTile
 	#this is only for now to show the start entrance
 	allRooms.get(startRoom).setTile(preload("res://Resources/T_Example.tres"), false)
@@ -37,23 +38,31 @@ func placeHolderSlots() -> void:
 		currentY += sizeOfTile
 
 func placeExit() ->Vector2i: 
-	var indicator = Vector2i(randi_range(0,dimensions.x-1), randi_range(0,dimensions.y-1))
+	var indicator = generatePOITilePlacement()
 	var exitTileData = Tile_Data.new()
 	var exitextra = preload("res://Resources/DungeonExtras/exit.tres")
 	var baseStyle = preload("res://Resources/baseStyle.tres")
 	exitTileData.extras.append(exitextra)
 	exitTileData.tileStyle = baseStyle
-	var selectedTile = allRooms.get(indicator) as MapTile 
-	exitTileData.currentEntrances =  generateEntrances()
-	while( !checkPlacement(indicator, exitTileData)):
-		exitTileData.currentEntrances = generateEntrances()
-	selectedTile.setTile(exitTileData, true)
-	selectedTile.pregeneratedTile = true
+	placePregeneratedTiles(exitTileData, indicator)
 	return indicator
 	
-	
+
+func generatePOITilePlacement() -> Vector2i:
+	return Vector2i(randi_range(0,dimensions.x-1), randi_range(0,dimensions.y-1))
+
 func generateEntrances() -> int:
 	return randi_range(1,15)
+
+func placePregeneratedTiles(tile: Tile_Data, pos: Vector2i) -> void:
+	var selectedTile = allRooms.get(pos) as MapTile 
+	tile.currentEntrances =  generateEntrances()
+	while( !checkPlacement(pos, tile)):
+		tile.currentEntrances = generateEntrances()
+	selectedTile.setTile(tile, true)
+	selectedTile.pregeneratedTile = true	
+	pass
+
 
 # checks if there is one "valid" exit. Works on bitmasks 
 func checkPlacement(pos: Vector2i, currEntrances: Tile_Data) -> bool:
@@ -83,3 +92,26 @@ func showDungeonEntrance() -> Vector2i:
 		else:
 			numX = dimensions.x-1
 	return Vector2i(numX, numY)
+
+func calcNumOfPOITiles() -> int:
+	return max(1, floor(dimensions.x * dimensions.y) / 12)
+
+#placing POI Tiles to encourage Player to keep exploring
+func placePOITiles(takenPos: Array[Vector2i]) -> void: 
+	
+	for num in calcNumOfPOITiles():
+		#maybe later we will have a selection, for now we only have chests
+		var poiTile: Tile_Data = Tile_Data.new()
+		var extra = preload("res://Resources/DungeonExtras/chest.tres")
+		var baseStyle = preload("res://Resources/baseStyle.tres")
+		poiTile.extras.append(extra)
+		poiTile.tileStyle = baseStyle
+		var pos: Vector2i = generatePOITilePlacement()
+		while takenPos.has(pos):
+			pos = generatePOITilePlacement()
+		takenPos.append(pos)
+		poiTile.currentEntrances = generateEntrances()
+		while !checkPlacement(pos, poiTile):
+			poiTile.currentEntrances = generateEntrances()
+		placePregeneratedTiles(poiTile, pos)
+	pass
